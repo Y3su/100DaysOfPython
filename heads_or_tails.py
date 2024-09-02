80508500734261aa9a23179ead3fe976d7fb4446
Y3su <lourencelariosa23@gmail.com>
2024-09-02 10:38:37 +0800

    first

---
+++ heads_or_tails.py
@@ -0,0 +1,11 @@
+import random
+
+heads_or_tails = random.randint(1,2)
+
+if heads_or_tails == 1:
+    print(heads_or_tails)
+    print("Heads")
+
+elif heads_or_tails == 2:
+    print(heads_or_tails)
+    print("Tails")
