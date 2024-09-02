938c2176410714650e89ba5a0af355abc7fd2832
Y3su <lourencelariosa23@gmail.com>
2024-09-02 14:09:11 +0800

    updated

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -3,9 +3,9 @@ import random
 heads_or_tails = random.randint(1,2)
 
 if heads_or_tails == 1:
-    print(heads_or_tails)
+    # print(heads_or_tails)
     print("Heads")
 
 elif heads_or_tails == 2:
-    print(heads_or_tails)
+    # print(heads_or_tails)
     print("Tails")
