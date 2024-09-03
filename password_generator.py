355141a332d2d8d348915fe5bc5f6810acfd1411
Y3su <lourencelariosa23@gmail.com>
2024-09-03 17:35:17 +0800

    test

--- password_generator.py
+++ password_generator.py
@@ -1,19 +1,3 @@
-# number = 0
-# for adding in range (1, 101):
-#     number += adding
-# print(number)
-
-# for num in range(1, 101):
-#
-#     if num % 3 == 0 and num % 5 == 0:
-#         print("FizzBuzz")
-#     elif num % 3 == 0:
-#         print("Fizz")
-#     elif num % 5 == 0:
-#         print("Buzz")
-#     else:
-#         print(num)
-
 import random
 
 letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -22,19 +6,25 @@ symbol = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 
-# input_letter = int(input("How many letters would you like in your password?\n"))
-# input_symbols = int(input("How many symbols would you like in your password?\n"))
-# input_numbers = int(input("How many numbers would you like in your password?\n"))
+input_letter = int(input("How many letters would you like in your password?\n"))
+input_symbol = int(input("How many symbols would you like in your password?\n"))
+input_number = int(input("How many numbers would you like in your password?\n"))
 
-password_shuffle = [letter, number, symbol]
-# random.shuffle(password_shuffle)
+random.shuffle(letter)
+random.shuffle(symbol)
+random.shuffle(number)
 
-random.shuffle(password_shuffle)
+selected_letter = letter[:input_letter]
+selected_symbol = letter[:input_symbol]
+selected_number = letter[:input_number]
 
-print(password_shuffle)
-# password_length = letter[:input_letter]
-#
-# password = ''.join(password_length)
-#
-# print(f"Here's your password: {password}")
+combined_shuffle = selected_letter + selected_symbol + selected_number
+
+random.shuffle(combined_shuffle)
+
+password = ''.join(combined_shuffle)
+
+print(f"Here's your password: {password}")
+
+
 
