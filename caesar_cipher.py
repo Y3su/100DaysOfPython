503de1bcbcaf925cca4c7f87879d467a29535375
Y3su <lourencelariosa23@gmail.com>
2024-09-18 22:10:29 +0800

    first

---
+++ caesar_cipher.py
@@ -0,0 +1,12 @@
+start = True
+while start:
+        choice = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+
+        if choice == "encode":
+            print("i")
+
+        elif choice == "decode":
+            print("h")
+
+
+
