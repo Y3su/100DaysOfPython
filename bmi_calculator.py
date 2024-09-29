52c308bfd782acdd2a9a4bcc205b957cd35a9888
Lourence Lariosa <150990528+Y3su@users.noreply.github.com>
2024-09-29 23:47:36 +0800

    updated

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,7 +1,6 @@
 height = float(input("What is your height in centimeters: ")) 
 weight = float(input("What is your weight in kilograms: ")) 
 
-#
 height_meters = height / 100
 bmi = weight / (height_meters * height_meters)
 
